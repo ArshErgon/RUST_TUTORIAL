441a670b34c9481388b93846a777263d2911e38b
ArshErgon <arshergon@gmail.com>
2022-12-08 10:17:12 +0430

    done conversion

--- rust_by_example/conversion/src/main.rs
+++ rust_by_example/conversion/src/main.rs
@@ -1,3 +1,51 @@
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    num: i32,
+}
+
+// these are small function
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { num: item }
+    }
+}
+
+struct Person {
+    name: String,
+    age: i32,
+}
+
+// to make u understand the use impl
+
+impl Person {
+    fn intro(&self) {
+        println!("My name is: {}", self.name);
+    }
+
+    fn tell_age(self) {
+        println!("My age is: {}", self.age);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_str: &str = "arsh";
+    let my_string = String::from(my_str).to_uppercase();
+    println!("{my_string} --- {my_str}");
+
+    let my_num = Number::from(20);
+    println!("{:?}", my_num);
+    println!("{:?}", my_num.num);
+
+    // a program for impl
+    let person = Person {
+        name: "Arsh".to_string(),
+        age: 20,
+    };
+    person.intro();
+    person.tell_age();
+
+    let new_num: i32 = "5".parse().unwrap();
+    println!("{new_num}");
 }
